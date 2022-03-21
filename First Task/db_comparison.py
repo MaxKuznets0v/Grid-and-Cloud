d1a4ce7db61a6cf46c31bb080b77a6ab0ae0b613
MaxKuznets0v <max.kuznetsov@yahoo.com>
2022-03-21 22:04:56 +0300

    add size as a parameter to post request

--- First Task/db_comparison.py
+++ First Task/db_comparison.py
@@ -48,7 +48,7 @@ def fill_up():
     db.insert_one(element)
 
 
-def make_move(db, insert: bool):
+def make_move(db, insert: bool, size=1):
     start = time.time()
     if insert:
         db.insert_many(generate(size))
@@ -59,23 +59,27 @@ def make_move(db, insert: bool):
 
 @app.route('/eval', methods=['POST', 'DELETE'])
 def eval():
+    data = request.json
+    try:
+        size = data['size']
+    except:
+        size = 1000
     ins = request.method == 'POST'
     local_client = pymongo.MongoClient('localhost', 27017)
     local_db = local_client['Database']['clients']
-    l_time = make_move(local_db, ins)
+    l_time = make_move(local_db, ins, size)
 
     web_client = pymongo.MongoClient('mongodb+srv://albert:hYDNdH56R0cyL8fN@cluster0.1kkjx.mongodb.net/Database?retryWrites=true&w=majority')
     web_db = web_client['Database']['clients']
-    w_time = make_move(web_db, ins)
+    w_time = make_move(web_db, ins, size)
 
     docker_client = pymongo.MongoClient('localhost', 27001)
     docker_db = docker_client['Database']['clients']
-    d_time = make_move(docker_db, ins)
+    d_time = make_move(docker_db, ins, size)
 
     return ({"l_time": l_time, "w_time": w_time, "d_time": d_time}, 200)
 
 
 if __name__ == '__main__':
-    size = 1000
     fill_up()
     app.run()
